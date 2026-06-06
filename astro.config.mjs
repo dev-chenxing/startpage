3969aa25e14c75dbf8b519075050ec3bd5c8238f
陈刑 <chenxing.io@outlook.com>
2026-06-06 14:17:19 +0800

    feat: add Astro's custom font support and update global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import react from "@astrojs/react";
 
@@ -16,6 +16,52 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
-
     integrations: [react(), mdx(), svelte()],
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: "JetBrains Mono",
+            cssVariable: "--font-jetbrains-mono",
+            optimizedFallbacks: false,
+            options: {
+                variants: [
+                    {
+                        src: ["./src/assets/fonts/JetBrainsMono-Regular.woff2"],
+                        weight: "normal",
+                        style: "normal",
+                    },
+                ],
+            },
+        },
+        {
+            provider: fontProviders.local(),
+            name: "Noto Sans CJK",
+            cssVariable: "--font-noto-sans-cjk",
+            optimizedFallbacks: false,
+            options: {
+                variants: [
+                    {
+                        src: ["./src/assets/fonts/NotoSansCJK-wght-400-900.ttf.woff2"],
+                        weight: "normal",
+                        style: "normal",
+                    },
+                ],
+            },
+        },
+        {
+            provider: fontProviders.local(),
+            name: "Symbols Nerd Font Mono",
+            cssVariable: "--font-symbols-nerd-font",
+            optimizedFallbacks: false,
+            options: {
+                variants: [
+                    {
+                        src: ["./src/assets/fonts/SymbolsNerdFontMono-Regular.ttf"],
+                        weight: "normal",
+                        style: "normal",
+                    },
+                ],
+            },
+        },
+    ],
 });
